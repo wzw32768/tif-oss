ed1ad5cd50a3696c9f6253fcac7e18369ee63502
aaa <a@a.com>
2023-05-11 15:50:45 +0800

    a

---
+++ src/main/java/a/App.java
@@ -0,0 +1,53 @@
+package a;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.wzw.tif.util.TifUtil;
+
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ByteArrayOutputStream byteArrayOutputStream = TifUtil.getFirstFrame(new File("D:/images/3.tif"));
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+        upload(byteArrayInputStream, "abc3/a.bmp");
+    }
+
+    public static void upload(InputStream inputStream, String fileName) throws Exception {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tMN5dB4Fug7YQSXVJ8E";
+        String accessKeySecret = "B2ptx8n67bu1s80ObEScNuFTxphDMz";
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "mussum2";
+        // 填写Object完整路径，例如exampledir/exampleobject.txt。Object完整路径中不能包含Bucket名称。
+        String objectName = fileName;
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+
+        try {
+            String content = "Hello OSS";
+            ossClient.putObject(bucketName, objectName, inputStream);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+}
